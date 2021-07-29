7ead1176acc8c9d3833ac206505b918086c765e1
Nathan Craddock <nzcraddock@gmail.com>
2021-07-29 15:19:35 -0600

    Memory leak and double-free checker
    The behavior is working, and the code is halfway through being cleaned
    up. I just figured I would commit now rather than wait.

---
+++ memory.cpp
@@ -0,0 +1,204 @@
+#include "memory.h"
+#undef new
+#undef delete
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+// for malloc and free
+#include <cstdlib>
+
+struct Record {
+    size_t size;
+    void *ptr;
+    AllocInfo info;
+    AllocInfo free_info;
+    bool complete = false;
+
+    Record(size_t size, void *ptr): size(size), ptr(ptr) {}
+
+    void leak_msg() {
+        std::cout << "  " << info;
+        std::cout << ": " << size << " bytes allocated here were never freed." << std::endl;
+    }
+};
+
+// track all allocations and frees
+struct MemTrack {
+    std::vector<void *> all_allocs;
+    size_t alloced;
+
+    std::vector<Record> allocated_records;
+    std::vector<Record> freed_records;
+    bool lock;
+
+    AllocInfo last_delete;
+
+    MemTrack() {
+        alloced = 0;
+        lock = false;
+    }
+
+    ~MemTrack() {
+        // free all memory for reals now
+        for (void *ptr : all_allocs) {
+            free(ptr);
+        }
+
+        if (!alloced) {
+            // all memory was freed
+            print_header();
+            std::cout << "no issues detected" << std::endl;
+            print_footer();
+            return;
+        }
+
+        print_header();
+        std::cout << "LEAK SUMMARY:" << std::endl;
+        std::cout << "  leaked: " << alloced << " bytes in " << allocated_records.size() << " allocations" << std::endl;
+        std::cout << std::endl;
+        std::cout << "LEAK DETAILS:" << std::endl;
+        for (Record r : allocated_records) {
+            if (r.complete) {
+                r.leak_msg();
+            }
+        }
+        print_footer();
+    }
+
+    void print_header() {
+        std::cout << std::endl;
+        std::cout << "---------- memory checker ----------" << std::endl;
+        std::cout << std::endl;
+    }
+
+    /* TODO: This may not be necessary with zybooks... */
+    void print_footer() {
+        std::cout << std::endl;
+        std::cout << "------------------------------------" << std::endl;
+        std::cout << std::endl;
+    }
+
+    void check_double_free(void *ptr) {
+        for (size_t i = 0; i < freed_records.size(); ++i) {
+            if (freed_records.at(i).ptr == ptr) {
+                // double free found
+
+                Record r = freed_records.at(i);
+                print_header();
+                std::cout << "DOUBLE-FREE SUMMARY:" << std::endl;
+                std::cout << "  attempted to free pointer " << ptr << " twice." << std::endl;
+                std::cout << std::endl;
+                std::cout << "DOUBLE-FREE DETAILS:" << std::endl;
+                std::cout << "  " << r.free_info << ": first freed here" << std::endl;
+                std::cout << "  " << last_delete << ": freed again here" << std::endl;
+                std::cout << "  " << r.info << ": allocated here" << std::endl;
+                print_footer();
+
+                // _Exit must be used because exit(1) still performs cleanup and the
+                // memory leak statements also print. We want to preserve the "abort
+                // on double-free" behavior from normal use of delete.
+                _Exit(1);
+            }
+        }
+    }
+
+    void remove_freed(void *ptr) {
+        if (lock) {
+            return;
+        }
+        lock = true;
+
+        for (size_t i = 0; i < allocated_records.size(); ++i) {
+            if (allocated_records.at(i).ptr == ptr) {
+                // track this record as freed for double-free warnings
+                allocated_records.at(i).free_info = last_delete;
+                freed_records.push_back(allocated_records.at(i));
+
+                alloced -= allocated_records.at(i).size;
+                allocated_records.erase(allocated_records.begin() + i);
+                break;
+            }
+        }
+
+        lock = false;
+    }
+
+    void check_address_reuse(void *ptr) {
+        for (size_t i = 0; i < freed_records.size(); ++i) {
+            if (freed_records.at(i).ptr == ptr) {
+                freed_records.erase(freed_records.begin() + i);
+                break;
+            }
+        }
+    }
+
+    void add_record(size_t size, void *ptr) {
+        if (lock) {
+            return;
+        }
+        lock = true;
+
+        check_address_reuse(ptr);
+        allocated_records.push_back(Record(size, ptr));
+        alloced += size;
+
+        lock = false;
+    }
+
+    void extend_record_info(const AllocInfo &info, void *ptr) {
+        for (size_t i = 0; i < allocated_records.size(); ++i) {
+            if (allocated_records.at(i).ptr == ptr) {
+                allocated_records.at(i).info = info;
+                allocated_records.at(i).complete = true;
+            }
+        }
+    }
+
+    void track_delete(AllocInfo info) {
+        last_delete = info;
+    }
+};
+
+static MemTrack tracker;
+
+// ptr has already been allocated and is in the tracker
+// this function exists to attach additional info to the record
+void SetAllocInfo(const AllocInfo &info, void *ptr) {
+    tracker.extend_record_info(info, ptr);
+}
+
+void track_delete(std::string filename, std::string function, int line) {
+    tracker.track_delete(AllocInfo(filename, function, line));
+}
+
+void *operator new(size_t size) {
+    void *ptr = malloc(size);
+    if (ptr == NULL) {
+        return NULL;
+    }
+    tracker.add_record(size, ptr);
+    return ptr;
+}
+
+void *operator new[](size_t size) {
+    void *ptr = malloc(size);
+    if (ptr == NULL) {
+        return NULL;
+    }
+    tracker.add_record(size, ptr);
+    return ptr;
+}
+
+void operator delete(void *ptr) {
+    tracker.check_double_free(ptr);
+    /* free(ptr); */
+    tracker.remove_freed(ptr);
+}
+
+void operator delete[](void *ptr) {
+    tracker.check_double_free(ptr);
+    /* free(ptr); */
+    tracker.remove_freed(ptr);
+}
---
+++ memory.h
@@ -0,0 +1,50 @@
+#ifndef _MEMORY_H
+#define _MEMORY_H
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+struct AllocInfo {
+    std::string file = "";
+    std::string function = "";
+    int line = 0;
+
+    AllocInfo() {}
+
+    AllocInfo(std::string filename, std::string function, int line):
+        file(filename), function(function), line(line) {}
+
+    friend std::ostream &operator<<(std::ostream &out, const AllocInfo &info) {
+        out << info.file << ":" << info.line << " in \"" << info.function << "\"";
+        return out;
+    }
+};
+
+void SetAllocInfo(const AllocInfo &info, void *ptr);
+
+template <class T> inline T *operator*(const AllocInfo &info, T *ptr) {
+    SetAllocInfo(info, ptr);
+    return ptr;
+}
+
+// This macro is the magic behind tracking the filename, function, and line
+// number for each allocation. New runs as per usual, and then the resulting
+// allocated pointer is multiplied with an AllocInfo object which has been
+// constructed with the current line info.
+// Because new returns a value, it is necessary to use this operator
+// overloading method to track the information on the current line. The
+// operator* function returns the pointer returned from new which allows this
+// to work anywhere new is allowed.
+#define new AllocInfo(__FILE__, __PRETTY_FUNCTION__, __LINE__) * new
+
+void track_delete(std::string filename, std::string function, int line);
+
+// Here we call a function to track the line that delete was called on before
+// we actually delete. This is used to warn about offending lines for double-
+// free errors. We cannot use the same operator* trick from above because
+// delete returns void. This means we can safely call a function in a
+// separate statement before running the delete operator.
+#define delete track_delete(__FILE__, __PRETTY_FUNCTION__, __LINE__); delete
+
+#endif
